e862eb9175ae73bee45a7aeda3c8545bd553b8b1
brunofreitas0907 <brunosilvafreitasti0907@gmail.com>
2025-03-14 07:53:52 -0300

    Add files via upload

--- exercicio-02.js
+++ exercicio-02.js
@@ -9,27 +9,39 @@ class Estoque
         this.quantidadeProduto = quantidadeProduto;
     }
 
-    adicionarProdutos()
+    adicionarProdutos(quantidadeAleatória)
     {
         this.quantidadeProduto += quantidadeAleatória;
         return this.quantidadeProduto;
     }
 
-    removerProdutos()
+    removerProdutos(quantidadeAleatória)
     {
         try
         {
-            this.quantidadeProduto -= quantidadeAleatória
-            return this.quantidadeProduto;
-        }catch{
-            console.log(`Deu erro DENOVOOOOOO`);
+            if(quantidadeAleatória > this.quantidadeProduto)
+            {
+                return `Não é possível remover ${quantidadeAleatória} unidades, pois o estoque atual é de ${this.quantidadeProduto} unidades.`;
+            }
+            else
+            {
+                this.quantidadeProduto -= quantidadeAleatória;
+                return `Ao remover produtos seu estoque atual será de ${this.quantidadeProduto} produtos!`;
+            }
+            
+        }
+        catch
+        {
+            console.error(`Deu erro DENOVOOOOOO ${error});
+            }` );
         }
     }
 }
 
+var estoqueProdutosRemove = new Estoque (2);
 var estoqueProdutos = new Estoque (50);
 var quantidadeAleatória = Math.floor(Math.random()*10);
 
-console.log(`Ao adicionar produtos seu estoque atual será de ${estoqueProdutos.adicionarProdutos()}`);
-console.log(`Ao remover produtos seu estoque atual será de ${estoqueProdutos.removerProdutos()}`);
-console.log(`O número sorteado foi o = ${quantidadeAleatória}`)
+console.log(`Ao adicionar produtos seu estoque atual será de ${estoqueProdutos.adicionarProdutos(quantidadeAleatória)} produtos!`);
+console.log(estoqueProdutosRemove.removerProdutos(quantidadeAleatória));
+console.log(`\nA quantidade sorteada foi de = ${quantidadeAleatória} produtos!`);
